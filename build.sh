f97d953ea5e344ee2271fd1fc7bca673e81e7ef1
arvin_j4848@protonmail.com <v4zha@v4zha_b0x.localdomain>
2022-05-18 15:04:57 +0530

    build

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 count=$1
-for((c=0;c<=$count;c++))
+for((c=0;c<=count;c++))
 do 
     echo "Building Level $c"
-    port=$((2020+$c))
+    port=$((2020+c))
     docker build ./lvl_$c -t wargame:lvl$c
     docker run -p $port:22 --rm -d --name clbattlelv$c --hostname clbattle wargame:lvl$c
     clear
