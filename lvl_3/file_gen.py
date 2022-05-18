2cc8586b629516f9883064705c947be75f05384f
v4zha <arvin_j4848@protonmail.com>
2022-05-18 22:42:14 +0530

    fix-lvl3-script

--- lvl_3/file_gen.py
+++ lvl_3/file_gen.py
@@ -1,7 +1,7 @@
 import random
 num_list=list(map(lambda x:str(x).zfill(5),range(1,30000)))
 random.shuffle(num_list)
-with open('lever_spell.txt','w') as file:
+with open('/home/lvl3/spell_book.txt','w') as file:
     for ele in num_list:
         if ele !='29997':
             file.write(f"{ele}-mandram : bhaja..bhaja..pada..pada..\n")
