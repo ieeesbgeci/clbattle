b220fc22aa8a55910f9e994972d4484b1efcd37d
v4zha <arvin_j4848@protonmail.com>
2022-05-18 15:46:22 +0530

    task-fix

--- lvl_0/scripts/task.py
+++ lvl_0/scripts/task.py
@@ -2,6 +2,6 @@ from story import storyTeller
 import sys
 if sys.argv[1] and sys.argv[1]=="fast":
     st=storyTeller(2,fast=True)
-else
+else:
     st=storyTeller(2)
 st.print_story()
--- lvl_1/scripts/task.py
+++ lvl_1/scripts/task.py
@@ -2,7 +2,7 @@ from story import storyTeller
 import sys
 if sys.argv[1] and sys.argv[1]=="fast":
     st=storyTeller(4,fast=True)
-else
+else:
     st=storyTeller(4)
 st.print_story()
 
