bdfae1c037c71d741c5b7b97ff6727879d44caf6
v4zha <arvin_j4848@protonmail.com>
2022-05-19 19:27:52 +0530

    tsk6-c-fix

--- lvl_6/task.c
+++ lvl_6/task.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(int argc,char* argv[]){
     int lock;
-    printf("Muthalakottayileekk swagatham\nUllil kadakkanamengil..AHmm.Ahm..\n")
+    printf("Muthalakottayileekk swagatham\nUllil kadakkanamengil..AHmm.Ahm..\n");
     printf("Lever inte lock thurakkanam \n");
     printf("lock >> :: ");
     scanf("%d",&lock);    
