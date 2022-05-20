bed81bc8dacab719284746e912b79fcb355ecac3
v4zha <arvin_j4848@protonmail.com>
2022-05-20 23:43:23 +0530

    build-fix

--- build_level.sh
+++ build_level.sh
@@ -8,11 +8,9 @@ if [ -z "$1" ]
 fi
 
 i=$1
-do 
-    echo "Building Level $i"
-    port=$((2020+i))
-    cp ./story.py ./lvl_$i/scripts/story.py
-    docker build ./lvl_$i -t wargame:lvl$i
-    docker run -p $port:22 --rm -d --name clbattlelv$i --hostname clbattle wargame:lvl$i
-    clear
-done 
+echo "Building Level $i"
+port=$((2020+i))
+cp ./story.py ./lvl_$i/scripts/story.py
+docker build ./lvl_$i -t wargame:lvl$i
+rm ./lvl_$i/scripts/story.py
+docker run -p $port:22 --rm -d --name clbattlelv$i --hostname clbattle wargame:lvl$i
