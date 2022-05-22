bd256eb72abcffb7f90c034dcc9aa8bd71d065b4
v4zha <arvin_j4848@protonmail.com>
2022-05-22 20:36:10 +0530

    lvl12

--- lvl12/task.sh
+++ lvl12/task.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-target='/home/lvl5'
-key_num=$(($(</dev/urandom tr -dc 1-9 |head -c6 )%2000))
-echo "target line : $key_num"
-for i  in {1..2000}
-do
-    if [ $i == $key_num ];then
-        echo "w9xR8Ia0hJz4J_{{v3gam_0od1Kk0}}_oLqnIChdlTJ1t" >>$target/escape.txt
-    else
-        txt=$(</dev/urandom tr -dc A-Za-z0-9 | head -c45)
-        echo $txt>>$target/escape.txt
-    fi
-done
-echo "done : )"
+target='/home/lvl12'
+echo -e "=============\ns4mbhaR4m\n=============\n" > $target/rahasyam
+mkdir $target/keys
+openssl genrsa -out $target/keys/private_key.pem 1024    
+openssl rsa -in $target/keys/private_key.pem -out $target/keys/public_key.pem -outform PEM -pubout
+mv $target/keys/private_key.pem $target/ak_private.pem
+cd $target
+openssl rsautl -encrypt -inkey $target/keys/public_key.pem -pubin -in rahasyam -out ak47.locked
+rm -rf $target/keys
+rm $target/rahasyam
+
