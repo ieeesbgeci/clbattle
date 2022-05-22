814680672cf086ba01168745ccda839ce7679caf
v4zha <arvin_j4848@protonmail.com>
2022-05-23 03:38:22 +0530

    ascii-14,15

--- lvl15/scripts/task.py
+++ lvl15/scripts/task.py
@@ -1,8 +1,8 @@
 from story import storyTeller 
 import sys
 if len(sys.argv)>1 and sys.argv[1]=="fast":
-    st=storyTeller(3,fast=True)
+    st=storyTeller(5,fast=True)
 else:
-    st=storyTeller(3)
+    st=storyTeller(5)
 st.print_story()
 
