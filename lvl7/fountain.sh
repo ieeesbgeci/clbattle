ffbf41ff09454e90ed236d085dd1edea973f9019
v4zha <arvin_j4848@protonmail.com>
2022-05-24 00:23:06 +0530

    7

--- lvl7/fountain.sh
+++ lvl7/fountain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "The Devine fountain : ) "
 if [ -f "/home/lvl7/fountain/vellam" ];then
-    if md5sum --status -c /usr/home/.config/scripts/fountain/vellam.md5;then
+    if md5sum --status -c /home/lvl7/.config/scripts/fountain/vellam.md5;then
         echo "Fountain activated : )"
         echo -e "=============\np4rzm_p0r1\n=============\n" 
     else
